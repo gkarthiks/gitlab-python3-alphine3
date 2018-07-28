3d58dda8cba9fb3339af37d998246817d7278c76
Karthikeyan Govindaraj <karthikeyan.govindaraj@blackrock.com>
2018-07-27 22:53:11 -0700

    changing the travis structure

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-REPO=gkarthics/gitlab-python3
+REPO=gkarthics/gitlab-python3-alpine3
 
 DOCKER_USERNAME=${DOCKER_USERNAME}
 DOCKER_PASSWORD=${DOCKER_PASSWORD}
@@ -14,7 +14,8 @@ docker build -f Dockerfile -t $REPO:$REVISION .
 docker tag $REPO:$REVISION $REPO:latest
 docker tag $REPO:$REVISION $REPO:1.$VERSION
 
-docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+# docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 docker push $REPO
 
